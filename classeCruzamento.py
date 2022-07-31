aaeea90b2ba7118b385b1d5eb4fcc3d9ea22c851
marcos-mv <vinicius_kof36@hotmail.com>
2022-07-31 00:23:41 -0300

    Creating Cruzamento class.

--- classeCruzamento.py
+++ classeCruzamento.py
@@ -1,15 +1,20 @@
-from msilib.schema import Class
 from gpiozero import LED, Button
 from time import sleep
 
-Class Cruzamento:
+class Cruzamento:
 
-    def __init__(self, vermelho1, amarelo1, verde1, vermelho2, amarelo2, verde2,
-        botaoPedestre1, botaoPedreste2, presenca1, presenca2, numeroCruzamento):
+    def __init__(self, SEMAFORO_1_VERMELHO, SEMAFORO_1_AMARELO,
+        SEMAFORO_1_VERDE, SEMAFORO_2_VERMELHO,
+        SEMAFORO_2_AMARELO, SEMAFORO_2_VERDE,
+        BOTAO_PEDESTRE_1, BOTAO_PEDESTRE_2, 
+        SENSOR_PASSAGEM_1, SENSOR_VELOCIDADE_1_A,
+        SENSOR_VELOCIDADE_1_B, SENSOR_PASSAGEM_2 ,
+        SENSOR_VELOCIDADE_2_A,
+        SENSOR_VELOCIDADE_2_B , numeroCruzamento):
     
         self.SEMAFORO_1_VERMELHO = SEMAFORO_1_VERMELHO,
         self.SEMAFORO_1_AMARELO = SEMAFORO_1_AMARELO,
-        self.SEMAFORO_1_VERMELHO = SEMAFORO_1_VERDE,
+        self.SEMAFORO_1_VERDE = SEMAFORO_1_VERDE,
 
         self.SEMAFORO_2_VERMELHO = SEMAFORO_2_VERMELHO,
         self.SEMAFORO_2_AMARELO = SEMAFORO_2_AMARELO,
@@ -25,9 +30,9 @@ Class Cruzamento:
         self.SENSOR_VELOCIDADE_1_B = SENSOR_VELOCIDADE_1_B,
 
         self.SENSOR_VELOCIDADE_2_A = SENSOR_VELOCIDADE_2_A,
-        self.SENSOR_VELOCIDADE_2_B = SENSOR_VELOCIDADE_2_B
-    )
+        self.SENSOR_VELOCIDADE_2_B = SENSOR_VELOCIDADE_2_B,
+        self.numeroCruzamento = numeroCruzamento
 
-cruzamento = Cruzamento(1,2,3,4,5,6,7,8,9,10,11)
+cruzamento = Cruzamento(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
 
 print(cruzamento.numeroCruzamento)
